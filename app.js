bd4f41b91fc101cc940c7da0c597151639818fef
David Odesola <faithodesola@gmail.com>
2020-02-29 14:32:06 +0800

    Login Page added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express=require("express");
+const bodyParser=require("body-parser");
+const ejs = require("ejs");
 const app = express();
 const port =8000;
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname+"/public"));
 
 app.get("/", function(req, res){
-    res.send("Hello World");
+    res.render("index");
 });
-
 app.listen(port, function(req, res){
-    console.log("Listening at port"+port);
+    console.log("Listening at port "+port);
 });
